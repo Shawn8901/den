ea0a6df9865c8a88aa6298fb88ddb4bcc0068f98
Shawn8901 <shawn8901@googlemail.com>
2025-12-29 21:09:40 +0100

    Refactor unfree aspect to use a module
    This allows the aspect to be included in other aspects and compose all unfree packages that are defined.

---
+++ modules/aspects/provides/unfree/unfree-predicate-builder.nix
@@ -0,0 +1,89 @@
+{ den, lib, ... }:
+let
+  description = ''
+    This is a private aspect always included in den.default.
+
+    It adds a module option that gathers all packages defined
+    in den._.unfree usages and declares a 
+    nixpkgs.config.allowUnfreePredicate for each class.
+
+  '';
+
+  unfreeComposableModule.options.unfree = {
+    packages = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
+  };
+
+  aspect = den.lib.parametric.exactly {
+    includes = [
+      (
+        { OS, host }:
+        let
+          unused = den.lib.take.unused OS;
+        in
+        {
+          ${host.class}.imports = [
+            unfreeComposableModule
+            (
+              { config, ... }:
+              {
+                nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) config.unfree.packages;
+              }
+            )
+          ];
+        }
+      )
+      (
+        {
+          OS,
+          HM,
+          user,
+          host,
+        }:
+        let
+          unused = den.lib.take.unused [
+            OS
+            HM
+          ];
+        in
+        {
+          ${user.class}.imports = [
+            unfreeComposableModule
+            (
+              { config, osConfig, ... }:
+              {
+                nixpkgs.config.allowUnfreePredicate =
+                  if (!osConfig.home-manager.useGlobalPkgs) then
+                    (pkg: builtins.elem (lib.getName pkg) config.unfree.packages)
+                  else
+                    null;
+              }
+            )
+          ];
+        }
+      )
+      (
+        { HM, home }:
+        let
+          unused = den.lib.take.unused HM;
+        in
+        {
+          ${home.class}.imports = [
+            unfreeComposableModule
+            (
+              { config, ... }:
+              {
+                nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) config.unfree.packages;
+              }
+            )
+          ];
+        }
+      )
+    ];
+  };
+in
+{
+  den.default.includes = [ aspect ];
+}
